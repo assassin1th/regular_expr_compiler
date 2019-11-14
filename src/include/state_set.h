1c3000d2061819417ebe579fb60c54c0e626db7e
Shushuev Artemiy <assassin1th@gmail.com>
2019-11-14 14:48:18 +0300

    add state_set header file

---
+++ src/include/state_set.h
@@ -0,0 +1,50 @@
+#ifndef STATE_SET_H_
+#define STATE_SET_H_
+
+/*
+ * Simple set structure
+ */
+typedef struct __s_set
+{
+	void *head;
+} state_set_t;
+
+/*
+ * Fucntion, produced empty set
+ */
+state_set_t *new_state_set ();
+
+/*
+ * Function, deleted set
+ */
+void free_state_set (state_set_t *__set)
+{
+	free (__set);
+}
+
+/*
+ * Function, copied set to new set
+ */
+state_set_t *copy_state_set (const state_set_t *__src_set);
+
+/*
+ * Function, updates set with new values
+ */
+states_set_t *update_state_set (state_set_t *__src_set, unsigned __state);
+
+/*
+ * Function, returns union of two sets from arguments
+ */
+state_set_t *union_state_set (const state_set_t *__first_src_set, const state_set_t *__second_src_set);
+
+/*
+ * Function, returns intersection of two sets from arguments
+ */
+state_set_t *intersection_state_set (const state_set_t *__first_src_set, const state_set_t *__second_src_set);
+
+/*
+ * Function, returns true if set1 == set2 else false
+ */
+int compare_state_set (const state_set_t *__first_src_set, const state_set_t *__second_src_set);
+
+#endif // STATE_SET_H_
