376249a20e79bd880dbf83aa42a37cc411b2de43
Shushuev Artemiy <assassin1th@gmail.com>
2019-11-22 02:00:56 +0300

    test(list): add tests for list

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,24 +5,5 @@ current_dir=`dirname "$absolute_fname"`
 
 if [ -n $1 ]
 then
-	for file in $current_dir/tests/$1/*
-	do
-		test_string=`line < "$file"`
-		expected_out=`tail -n +2 < "$file"`
-		out=`$current_dir/bin/$1 $test_string`
-
-		echo "$current_dir/bin/$1 $test_string"
-		if [ `echo $?` != "0" ]
-		then
-			echo "args: $test_string"
-			echo "Bad exit code"
-		fi
-
-		if [ "$expected_out" != "$out" ]
-		then
-			echo "Wrong output"
-			echo "expected: $expected_out"
-			echo "recieved: $out"
-		fi
-	done
+	python3 $current_dir/run_tests.py $1
 fi
